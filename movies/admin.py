4112602f82d00b9a6ff881b4a573685692655851
Shakib Hossain <shakib609@gmail.com>
2018-09-05 16:22:24 +0600

    Create Movie, Comment model and generate migrations

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie, Comment
+
+
+admin.site.register(Movie)
+admin.site.register(Comment)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+import uuid
+
+
+def generate_upload_location(instance, filename):
+    random = str(uuid.uuid4())
+    return 'mov_{}/{}.jpg'.format(instance.id, random)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=256)
+    plot = models.TextField()
+    poster_image = models.ImageField(
+        upload_to=generate_upload_location,
+        null=True,
+        blank=True)
+    year = models.IntegerField()
+    imdb_rating = models.FloatField(default=0)
+
+    def __str__(self):
+        return '{}({})'.format(self.title, self.year)
+
+
+class Comment(models.Model):
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return '{}... - {}'.format(self.text[:5], self.created_by.username)
