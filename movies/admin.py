654a4611b1fe0b9f5639c9f6ee715fcf099acc4c
Shakib Hossain <shakib609@gmail.com>
2018-09-05 23:19:16 +0600

    Create DownloadLinkForm and modify Movie admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
 from .models import Movie, Comment, DownloadLink, Genre
+from .forms import DownloadLinkForm
 
 
-admin.site.register(Movie)
-admin.site.register(Comment)
+class DownloadLinkInline(admin.TabularInline):
+    model = DownloadLink
+    form = DownloadLinkForm
+    extra = 0
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    inlines = [DownloadLinkInline]
+
+
 admin.site.register(DownloadLink)
+admin.site.register(Comment)
 admin.site.register(Genre)
