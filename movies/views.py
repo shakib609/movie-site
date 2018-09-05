577e1c380716589cffb75cc49a103815907337be
Shakib Hossain <shakib609@gmail.com>
2018-09-06 03:53:21 +0600

    Partially finish Movie Detail page

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Movie
 
@@ -9,3 +9,17 @@ class HomePageView(ListView):
 
     def get_queryset(self):
         return Movie.objects.all()[:8]
+
+
+class MovieDetailView(DetailView):
+    template_name = 'movies/detail.html'
+    context_object_name = 'movie'
+    queryset = Movie.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['genres'] = self.object.genres.all()
+        context['download_links'] = self.object.download_links.all()
+        context['random_movies'] = Movie.objects.order_by('?')[:4]
+        context['comments'] = self.object.comments.order_by('-created_at').all()
+        return context
